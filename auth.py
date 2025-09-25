6d4231b9c4aec47c4de945de01d5fb36fc0cf80b
Ese Amadasun <amadasunese@gmail.com>
2025-09-25 12:39:37 +0000

    updated

--- auth.py
+++ auth.py
@@ -8,11 +8,11 @@ def admin_required(f):
         if not current_user.is_authenticated:
             flash("You need to log in first.", "warning")
             return redirect(url_for("auth.login"))
-        
+
         if not getattr(current_user, "is_admin", False):
             flash("You do not have permission to access this page.", "danger")
-            return render_template("403.html"), 403  # Redirect to custom error page
+            return render_template("403.html"), 403
 
         return f(*args, **kwargs)
-    
+
     return decorated_function
