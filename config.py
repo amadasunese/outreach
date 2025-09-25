bd8279b3ec3e84856ba49c9225b8557a041665a5
Ese Amadasun <amadasunese@gmail.com>
2025-09-25 12:21:47 +0000

    updated config file

--- config.py
+++ config.py
@@ -1,28 +1,7 @@
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# class Config:
-#     SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
-#     # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///students.db')
-#     # SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-#     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://gpimamagementsys:Gentle&&08058767059@gpimamagementsystem.mysql.pythonanywhere-services.com/gpimamagementsys$default"
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-# import os
-
-# class Config:
-#     SECRET_KEY = os.getenv('SECRET_KEY')
-#     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # only for local development
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
